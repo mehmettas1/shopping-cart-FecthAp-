df206722d7bac13715d2c104b456637539659b76
mehmet <tas349873@gmail.com>
2022-11-27 17:43:06 +0300

    const js

---
+++ script.js
@@ -0,0 +1,6 @@
+const cartBtn =document.querySelector(".cart-btn");
+const clearCartBtn =document.querySelector(".btn-clear");
+const cartItems =document.querySelector(".cart-items");
+const cartTotal =document.querySelector(".total-value");
+const cartContent =document.querySelector(".cart-content");
+const productsDOM =document.querySelector("#products-dom");
